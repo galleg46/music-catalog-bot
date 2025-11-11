82af5e9324bfab4295853db727e530382389d251
Anthony Gallegos <galleg46@uwm.edu>
2025-11-11 13:00:11 -0600

    Discord Bot creation initial code for the bot to work.
    This includes:
     - a utils.js file that contains a DiscordRequest function to make requests to the Discord API. Another function that installs global commands to the bots library of slash commands
     - a commands.js file that holds the current gloabl slash commands for the bot.
     - an app.js file runs the bot and makes calls to the Interactions endpoint to send http requests inorder to handle slash command requests

---
+++ app.js
@@ -0,0 +1,99 @@
+import 'dotenv/config';
+import express from 'express';
+import {
+    InteractionResponseFlags,
+    InteractionResponseType,
+    InteractionType,
+    MessageComponentTypes,
+    verifyKeyMiddleware,
+} from 'discord-interactions';
+import { getRandomEmoji, DiscordRequest } from './utils.js';
+
+// Create an express app
+const app = express();
+// Get port, or default to 3000
+const PORT = process.env.PORT || 3000;
+
+/**
+ * Interactions endpoint URL where Discord will send HTTP requests
+ * Parse request body and verifies incoming requests using discord-interactions package
+ */
+app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
+    // Interaction id, type, data and channel_id
+    const { id, type, data, channel_id } = req.body;
+
+    /**
+     * Handle verification requests
+     */
+    if (type === InteractionType.PING) {
+        return res.send({ type: InteractionResponseType.PONG });
+    }
+
+    /**
+     * Handle slash command requests
+     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+     */
+    if (type === InteractionType.APPLICATION_COMMAND) {
+        const { name } = data;
+
+        // "test" command
+        if (name === 'test') {
+            // Send a message into the channel where command was triggered from
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+                    components: [
+                        {
+                            type: MessageComponentTypes.TEXT_DISPLAY,
+                            // Fetches a random emoji to send from a helper function
+                            content: `hello world ${getRandomEmoji()}`
+                        }
+                    ]
+                },
+            });
+        }
+
+        if (name === 'clean_up_purchases') {
+            const messages = await DiscordRequest(`channels/${channel_id}/messages`, {
+                method: 'GET'
+            }).then(function (response) {
+                return response.json();
+            });
+
+            const filteredMessages = messages.filter(function (message) {
+                return message.reactions?.some((reaction) => reaction.emoji.name === '🛍️');
+            });
+
+            console.log(filteredMessages);
+            for (const message of filteredMessages) {
+                await DiscordRequest(`channels/${channel_id}/messages/${message.id}`, {
+                    method: 'DELETE'
+                });
+            }
+
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+                    components: [
+                        {
+                            type: MessageComponentTypes.TEXT_DISPLAY,
+                            content: 'purchased links have been deleted'
+                        }
+                    ]
+                }
+            })
+        }
+
+        console.error(`unknown command: ${name}`);
+        return res.status(400).json({ error: 'unknown command' });
+    }
+
+    console.error('unknown interaction type', type);
+    return res.status(400).json({ error: 'unknown interaction type' });
+});
+
+app.listen(PORT, () => {
+    console.log('Listening on port', PORT);
+});
